cd19d7cd3caae037c4ca803b2c4e3e732c23cd95
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-07-20 12:56:26 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,10 @@
 import 'package:flutter/material.dart';
+import 'game_screen.dart';
 
 void main() {
-  runApp(const HundredGame());
-}
-
-class HundredGame extends StatelessWidget {
-  const HundredGame({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Hundred Game')),
-        body: const Center(child: GameCounter()),
-      ),
-    );
-  }
-}
-
-class GameCounter extends StatefulWidget {
-  const GameCounter({super.key});
-  @override
-  State<GameCounter> createState() => _GameCounterState();
-}
-
-class _GameCounterState extends State<GameCounter> {
-  int _counter = 0;
-
-  void _increment() {
-    setState(() {
-      if (_counter < 100) _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text('Score: $_counter', style: const TextStyle(fontSize: 40)),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: _increment,
-          child: const Text('Tap to Count'),
-        ),
-      ],
-    );
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: GameScreen(),
+  ));
 }
