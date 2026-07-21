221b0088b195180ca9611c77b6344e9944bedad5
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-07-21 11:37:47 +0530

    Update game_models.dart

--- lib/game_models.dart
+++ lib/game_models.dart
@@ -1,15 +1,39 @@
-enum GameMode { online, offline }
+enum GameMode { offline, online }
 
 class Player {
   final String id;
-  final String name;
+  String name;
+  String avatar;
   List<int> hand;
   int currentScore;
 
   Player({
     required this.id,
     required this.name,
+    this.avatar = "👑",
     required this.hand,
     this.currentScore = 0,
   });
+}
+
+class UserProfile {
+  String name;
+  String avatar;
+  int totalMatches;
+  int totalWins;
+  int xp;
+  int level;
+  int highScore;
+
+  UserProfile({
+    this.name = "Guest Player",
+    this.avatar = "👑",
+    this.totalMatches = 0,
+    this.totalWins = 0,
+    this.xp = 0,
+    this.level = 1,
+    this.highScore = 0,
+  });
+
+  double get winRate => totalMatches > 0 ? ((totalWins / totalMatches) * 100) : 0.0;
 }
