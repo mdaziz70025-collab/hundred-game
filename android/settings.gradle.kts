09cfc92ce45cc83aa698c7f29dc392a26d840879
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-07-27 18:01:59 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -22,7 +22,7 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false // 👈 Version ko 2.1.0 kar diya hai
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false // 👈 Target metadata 2.3.0 ke sath match kar diya hai
 }
 
 include(":app")
