9904baac68cccd3ac3ecfa752e9d539ee74ae88b
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-08-04 23:16:58 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -21,9 +21,9 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false // 👈 Firebase Google Services Plugin Added
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
 include(":app")
