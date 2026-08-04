05cd5b91638ae9fc619fa1b0556d976f77ce465e
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-08-04 21:30:47 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+// 1. Buildscript block HAMESHA sabse upar hona chahiye
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
+
+// 2. All projects repositories
 allprojects {
     repositories {
         google()
@@ -5,6 +17,7 @@ allprojects {
     }
 }
 
+// 3. Custom build directory logic (Flutter default setup)
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
@@ -17,17 +30,7 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// 4. Clean task
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-// 👈 Google Services Plugin Classpath Added Here
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.1")
-    }
 }
