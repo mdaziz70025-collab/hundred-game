79530ad3ca92c3287ad8f8e3e86446acc74fceb1
mdaziz70025-collab <mdaziz70025@gmail.com>
2026-08-05 16:13:24 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -189,7 +189,7 @@ class _HomeScreenState extends State<HomeScreen> {
     } catch (e) {
       try {
         final AccessToken? accessToken = await FacebookAuth.instance.accessToken;
-        if (accessToken != null && accessToken.expires.isAfter(DateTime.now())) {
+        if (accessToken != null && accessToken.tokenString.isNotEmpty) {
           final OAuthCredential credential = FacebookAuthProvider.credential(accessToken.tokenString);
           UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
 
